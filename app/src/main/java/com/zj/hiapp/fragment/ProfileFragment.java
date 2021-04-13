21bcef37fa3bd1472a5614f97cac096b98dfa274
zhangjin <15659515613@163.com>
2021-04-13 17:50:55 +0800

    1.ImageView加载图片扩展

--- app/src/main/java/com/zj/hiapp/fragment/ProfileFragment.java
+++ app/src/main/java/com/zj/hiapp/fragment/ProfileFragment.java
@@ -1,5 +1,15 @@
 package com.zj.hiapp.fragment;
 
+import android.graphics.Typeface;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+
+import androidx.core.content.ContextCompat;
+
 import com.zj.common.ui.component.HiBaseFragment;
 import com.zj.hiapp.R;
 
@@ -7,5 +17,29 @@ public class ProfileFragment extends HiBaseFragment {
     @Override
     public int getLayoutId() {
         return R.layout.fragment_profile_page;
+    }
+
+    private CharSequence spannableTabItem(int topText, String bottomText) {
+        String spanStr = String.valueOf(topText);
+        SpannableStringBuilder ssb = new SpannableStringBuilder();
+        SpannableString topSpannable = new SpannableString(spanStr);
+
+        int spanFlag = Spannable.SPAN_EXCLUSIVE_EXCLUSIVE;
+
+        topSpannable.setSpan(
+                new ForegroundColorSpan(ContextCompat.getColor(getContext(), R.color.color_000)),
+                0,
+                topSpannable.length(),
+                spanFlag
+        );
+        topSpannable.setSpan(
+                new AbsoluteSizeSpan(18, true),
+                0,
+                topSpannable.length(),
+                spanFlag);
+        topSpannable.setSpan(new StyleSpan(Typeface.BOLD), 0, topSpannable.length(), spanFlag);
+        ssb.append(topSpannable);
+        ssb.append(bottomText);
+        return ssb;
     }
 }
---
+++ common/src/main/java/com/zj/common/ui/view/ImageViewExts.kt
@@ -0,0 +1,71 @@
+package com.zj.common.ui.view
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+fun ImageView.loadUrl(url: String) {
+    Glide.with(context).load(url).into(this)
+}
+
+fun ImageView.loadCircle(url: String) {
+    Glide.with(this).load(url)
+        .transform(CircleCrop()).into(this)
+}
+
+//巨坑，glide 的 图片裁剪 和 imageview scaleType 有冲突。 centerCrop .
+fun ImageView.loadCorner(url: String, corner: Int) {
+    Glide.with(this).load(url).transform(CenterCrop(), RoundedCorners(corner)).into(this)
+}
+
+fun ImageView.loadCircleBorder(
+    url: String,
+    borderWidth: Float = 0f,
+    borderColor: Int = Color.WHITE
+) {
+
+    Glide.with(this).load(url).transform(CircleBorderTransform(borderWidth, borderColor)).into(this)
+
+}
+
+class CircleBorderTransform(private val borderWidth: Float, borderColor: Int) : CircleCrop() {
+    private var borderPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    init {
+        borderPaint.color = borderColor
+        borderPaint.style = Paint.Style.STROKE
+        borderPaint.strokeWidth = borderWidth
+    }
+
+    override fun transform(
+        pool: BitmapPool,
+        toTransform: Bitmap,
+        outWidth: Int,
+        outHeight: Int
+    ): Bitmap {
+        val transform = super.transform(pool, toTransform, outWidth, outHeight)
+        val canvas = Canvas(transform)
+
+        val halfWidth = outWidth / 2.toFloat()
+        val halfHeight = outHeight / 2.toFloat()
+
+
+        canvas.drawCircle(
+            halfWidth,
+            halfHeight,
+            halfWidth.coerceAtMost(halfHeight) - borderWidth / 2,
+            borderPaint
+        )
+
+        canvas.setBitmap(null)
+
+        return transform
+    }
+}
